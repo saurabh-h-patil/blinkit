7e3430c690164a813119fd657a768f3df012b304
saurabhthakre1721999 <saurabhthakre1721999@gmail.com>
2024-03-12 19:27:44 +0530

    fix footer

--- blinkit/src/pages/home/home.jsx
+++ blinkit/src/pages/home/home.jsx
@@ -1,9 +1,11 @@
+import Footer from "../../components/footer/footer";
 import Header from "../../components/header/header";
 
 const Home = () => {
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
       <Header />
+      <Footer />
     </div>
   );
 };
