dd85bf0df0ef0f5601776f548a5b6ffba5f00b06
saurabhthakre1721999 <saurabhthakre1721999@gmail.com>
2024-04-01 16:33:33 +0530

    fix maping

--- blinkit/src/components/productListRow/productrow.jsx
+++ blinkit/src/components/productListRow/productrow.jsx
@@ -1,10 +1,30 @@
 import { Link } from "react-router-dom";
 
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 import Items from "./productListAtoms/items";
+
 const ProductRow = ({ products }) => {
-  console.log("products", products);
-  console.log("row products", products);
-  const Allproduct = products.objects[0];
+  console.log("productsproducts", products);
+  const Allproduct = products.objects[0].data.products;
+
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 6,
+      slidesToSlide: 3, // optional, default to 1.
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+      slidesToSlide: 2, // optional, default to 1.
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+      slidesToSlide: 1, // optional, default to 1.
+    },
+  };
   return (
     <>
       <div
@@ -19,16 +39,15 @@ const ProductRow = ({ products }) => {
       >
         <div style={{ maxWidth: "1280px" }}>
           {<h1>{products.data.title}</h1>}
-          <div
-            style={{
-              display: "flex",
-              overflow: "scroll",
-            }}
-          >
-            {Allproduct.data.products.map((item) => {
-              return <Items item={item} />;
+          <Carousel responsive={responsive}>
+            {Allproduct.map((item) => {
+              return (
+                <Link to={`/details:${item.id}`}>
+                  <Items item={item} />{" "}
+                </Link>
+              );
             })}
-          </div>
+          </Carousel>
         </div>
       </div>
     </>
